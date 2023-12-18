a2e816713acb11f71194b9dd6398e79c3dae12a0
Alex <bodnaro926@gmail.com>
2023-12-18 21:10:55 +0200

    changes v1

--- js/forCoinGradient.js
+++ js/forCoinGradient.js
@@ -1,4 +1,34 @@
-/*скріпт який регулює видимість градієнту на орбіті монет
+/*скріпт який регулює видимість градієнту на орбіті монет*/
+
+(function() {
+    function updateOrbitClass() {
+        const coinOrbitElement = document.getElementById('coinOrbit');
+        const orbitBottom = coinOrbitElement.getBoundingClientRect().bottom;
+        const windowHeight = window.innerHeight;
+        const windowWidth = window.innerWidth;
+
+        if (windowWidth <= 574) {
+            const coinOrbitTop = coinOrbitElement.getBoundingClientRect().top;
+            if (orbitBottom < windowHeight && coinOrbitTop < windowHeight * 0.5) {
+                coinOrbitElement.classList.add('active');
+            } else {
+                coinOrbitElement.classList.remove('active');
+            }
+        } else {
+            if (orbitBottom < windowHeight) {
+                coinOrbitElement.classList.add('active');
+            } else {
+                coinOrbitElement.classList.remove('active');
+            }
+        }
+    }
+
+    window.addEventListener('scroll', updateOrbitClass);
+    window.addEventListener('load', updateOrbitClass);
+    window.addEventListener('resize', updateOrbitClass);
+})();
+
+
 
 /*(function() {
     window.addEventListener('scroll', function() {
